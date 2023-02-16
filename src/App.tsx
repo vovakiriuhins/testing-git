51e7638c0ac4c190530a3e49775bf3675ef2ed3b
vovakiriuhins <vova11999977@gmail.com>
2023-02-16 20:34:39 +0300

    Commit - 1

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,14 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>Lorem Lorem ipsum dolor.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+            Learn React
         </a>
       </header>
     </div>
